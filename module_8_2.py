2e2a1844bb59fd30b6549295a3fc1c58d730f0ee
nina1982-ops <ya.nina.82@yandex.ru>
2024-09-15 09:29:36 +0300

    Add files via upload

--- module_8_2.py
+++ module_8_2.py
@@ -1,34 +1,3 @@
-# def personal_sum(numbers):
-#     result = 0
-#     incorrect_data = 0
-#     for num in numbers:
-#         try:
-#             result += numbers
-#         except TypeError:
-#             incorrect_data += 1
-#     return result, incorrect_data
-#
-# def calculate_average(numbers):
-#
-#         try:
-#             sum_numbers, incorrect_data = personal_sum(numbers)
-#             if len(numbers) > 0:
-#                 return sum_numbers / len(numbers)
-#             else:
-#                 return None
-#
-# try:
-#
-# except TypeError:
-#     print('В numbers записан некорректный тип данных'))
-#
-#
-#     # Пример выполнения программы:
-#     print(f'Результат 1: {calculate_average("1, 2, 3")}') # Строка перебирается, но каждый символ - строковый тип
-#     print(f'Результат 2: {calculate_average([1, "Строка", 3, "Ещё Строка"])}') # Учитываются только 1 и 3
-#     print(f'Результат 3: {calculate_average(567)}') # Передана не коллекция
-#     print(f'Результат 4: {calculate_average([42, 15, 36, 13])}') # Всё должно работать
-
 
 def personal_sum(numbers):
     result = 0
@@ -43,14 +12,24 @@ def personal_sum(numbers):
     return result, incorrect_data
 def calculate_average(numbers):
     try:
-        sum_numbers, incorrect_data = personal_sum(numbers)
-        return sum_numbers / incorrect_data if incorrect_data > 0 else 0
+        total, incorrect_data = personal_sum(numbers)
+
+        count = len(numbers) - incorrect_data
+        if count == 0:
+            raise ZeroDivisionError
+        return total / count
+
+
+    except ZeroDivisionError:
+        return 0
     except TypeError:
-        print('В numbers записан некорректный тип данных')
-        return None
+        return "В numbers записан некоректный тип данных"
 
 # Пример выполнения программы:
 print(f'Результат 1: {calculate_average("1, 2, 3")}')  # Строка перебирается, но каждый символ - строковый тип
 print(f'Результат 2: {calculate_average([1, "Строка", 3, "Ещё Строка"])}')  # Учитываются только 1 и 3
 print(f'Результат 3: {calculate_average(567)}')  # Передана не коллекция
 print(f'Результат 4: {calculate_average([42, 15, 36, 13])}')  # Всё должно работать
+
+
+
